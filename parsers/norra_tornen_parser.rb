4c2b5063dd25990493dc5ae552eff56870188db8
buffpojken <daniel@sykewarrior.com>
2015-04-15 13:50:42 +0200

    testing

--- parsers/norra_tornen_parser.rb
+++ parsers/norra_tornen_parser.rb
@@ -1,7 +1,8 @@
 class NorraTornenParser < SmallDefinitionParser
 
 	def self.endpoint_url
-		"http://nt.spektradesign.se/wp-content/themes/Norra-Tornen/vitecSync.php"
+#		"http://nt.spektradesign.se/wp-content/themes/Norra-Tornen/vitecSync.php"
+		"http://requestb.in/1fecj8t1"
 	end
 
 end
