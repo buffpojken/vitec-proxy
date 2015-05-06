879608152d764506670301f54742149fe2c036d1
buffpojken <daniel@sykewarrior.com>
2015-05-06 22:45:33 +0200

    activated image-parsing

--- parsers/norra_tornen_parser.rb
+++ parsers/norra_tornen_parser.rb
@@ -5,9 +5,21 @@ class NorraTornenParser < SmallDefinitionParser
 #		"http://requestb.in/1fecj8t1"
 	end
 
-	def self.parse(result, status, index)
-		ap result.inspect
-		super
+	def self.parse(result, status, index)		
+		objectData = super
+		image_list = result[:bilder][:bild]
+		if image_list && image_list.is_a?(Array)
+			image_list.each do |image_package|
+				if ["view", "preview"].include?(image_package[:kategori])
+					url = "http://fastighet.capitex.se/CapitexResources/Capitex.Datalager.DBFile/Capitex.Datalager.DBFile.dbfile.aspx?g=#{image_package[:guid]}&t=CBild"
+					objectData[image_package[:kategori]] = url
+				else
+					next
+				end
+			end
+			ap objectData
+		end
+		objectData
 	end
 
 end
