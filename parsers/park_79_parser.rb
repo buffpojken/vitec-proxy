6de2bdb3e8edaf7d5154e9b8987aa86bd9ed750a
buffpojken <daniel@sykewarrior.com>
2015-08-19 16:48:23 +0200

    updated 79park

--- parsers/park_79_parser.rb
+++ parsers/park_79_parser.rb
@@ -5,18 +5,25 @@ class Park79Parser < SmallDefinitionParser
 	end
 
 	def self.parse(result, status, index)		
+		ap result
 		objectData = super
-		image_list = result[:bilder][:bild]
-		if image_list && image_list.is_a?(Array)
-			image_list.each do |image_package|
-				if ["preview"].include?(image_package[:kategori])
-					url = "http://fastighet.capitex.se/CapitexResources/Capitex.Datalager.DBFile/Capitex.Datalager.DBFile.dbfile.aspx?g=#{image_package[:guid]}&t=CBild"
-					objectData[image_package[:kategori]] = url
-				else
-					next
+		if result[:bilder]
+			image_list = result[:bilder][:bild]
+			if image_list && image_list.is_a?(Array)
+				image_list.each do |image_package|
+					if ["preview"].include?(image_package[:kategori])
+						url = "http://fastighet.capitex.se/CapitexResources/Capitex.Datalager.DBFile/Capitex.Datalager.DBFile.dbfile.aspx?g=#{image_package[:guid]}&t=CBild"
+						objectData[image_package[:kategori]] = url
+					else
+						next
+					end
 				end
-			end
+			elsif image_list && image_list.is_a?(Hash)
+				url = "http://fastighet.capitex.se/CapitexResources/Capitex.Datalager.DBFile/Capitex.Datalager.DBFile.dbfile.aspx?g=#{image_list[:guid]}&t=CBild"
+                                        objectData[image_list[:kategori]] = url
+			end	
 		end
+	ap objectData
 		objectData
 	end
 
